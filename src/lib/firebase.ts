74b03cd35ff95908fb7c45c147360310b0799861
Mike Wallsten <mikewallsten@gmail.com>
2025-10-03 05:34:42 +0000

    STORE THESE
    Service Account.
    
    var admin = require("firebase-admin");
    
    v

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD1-yMpv2_uv13l_leUYyVCBscanqUGORQ",
   authDomain: "studio-3755263487-6db2c.firebaseapp.com",
   projectId: "studio-3755263487-6db2c",
-  storageBucket: "studio-3755263487-6db2c.firebasestorage.app",
+  storageBucket: "studio-3755263487-6db2c.appspot.com",
   messagingSenderId: "264861209691",
   appId: "1:264861209691:web:da998a83d059d2bbbe9b7e"
 };
